47cd35079654c1c60dee63647e713312895116fd
Aratrik Roy <121568780+aratrik1210@users.noreply.github.com>
2024-05-22 19:33:12 +0530

    code 3.py

--- code 3.py
+++ code 3.py
@@ -1,4 +1,4 @@
-#ask for birth year and return age
-birth_year = input('Birth year: ')
+#ask for birth year and return age (type conversion)
+birth_year = input('Birth year: ')   
 age = 2024 - int(birth_year)
 print(age)
